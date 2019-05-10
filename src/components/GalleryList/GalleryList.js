64326b175e740f83140e059001aeee5887861ccf
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-10 16:44:48 -0500

    adding GalleryList

---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class GalleryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pictures: 
+    }
+   
+  render() {
+    return (
+      <div>
+          <div>
+              
+          </div>
+          <div>
+    
+          </div>
+      </div>
+    );
+  }
+}
+
+export default GalleryList;
