1ec11761e326de0a15d1de7af8a084d1ee8f086a
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-10 14:17:51 -0500

    imported Axios

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    newImages: {
+    },
+    images: [],
+  }
+  componentDidMount() {
+    console.log('componentDidMount works');
+    this.getImages();
+  }
+  getImages() {
+    axios({
+      method: 'GET',
+      url: '/images',
+    })
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        images: response.data,
+      })
+    });
+  }
+
+  
+
   render() {
     return (
       <div className="App">
@@ -10,6 +35,8 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
+        <galleryItems />
+
         <img src="images/goat_small.jpg"/>
         <img src="images/chick.jpg"/>
         <img src="images/duckling.jpg"/>
